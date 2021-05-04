d27a839bc28c13f51b03d8170a1790c63c05cf08
Fernanda <fernandamaia1001@gmail.com>
2021-05-04 19:42:40 -0300

    Project create

---
+++ backend/src/main/java/com/unitins/controleglicemia/ControleglicemiaApplication.java
@@ -0,0 +1,13 @@
+package com.unitins.controleglicemia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleglicemiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleglicemiaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/unitins/controleglicemia/ControleglicemiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitins.controleglicemia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleglicemiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
